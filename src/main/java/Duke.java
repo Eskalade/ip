82a37500a9b9c72be8d3679452c21d6834275bf0
Vincent Peh <medalking2013@gmail.com>
2026-08-21 00:48:57 +0800

    Implement Level-0: Rename chatbot to NutriSoy, greet, and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
 public class Duke {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(banner);
+        String logo = """
+                _   __      __         _  _____              
+               / | / /_  __/ /_________(_)/ ___/____  __  __ 
+              /  |/ / / / / __/ ___/  _  /\\__ \\/ __ \\/ / / / 
+             / /|  / /_/ / /_/ /   / / / /___/ / /_/ / /_/ /  
+            /_/ |_/\\__,_/\\__/_/   /_/ /_//____/\\____/\\__, /   
+                                                    /____/    
+            """;
+
+        String divider = "____________________________________________________________";
+
+        // Greet
+        System.out.println(divider);
+        System.out.print(logo);
+        System.out.println(" Hello! I'm NutriSoy");
+        System.out.println(" What can I do for you?");
+        System.out.println(divider);
+
+        // Exit
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(divider);
     }
 }
