52fc1cc3084f038f5e6c33ad249605a06f0a878e
Vincent Peh <medalking2013@gmail.com>
2026-08-28 00:36:29 +0800

    Implement A-JUnit: Add unit tests for TaskList and Parser classes

---
+++ src/test/java/nutrisoy/parser/ParserTest.java
@@ -0,0 +1,38 @@
+package nutrisoy.parser;
+
+import org.junit.jupiter.api.Test;
+import nutrisoy.command.Command;
+import nutrisoy.command.TodoCommand;
+import nutrisoy.command.ExitCommand;
+import nutrisoy.exception.DukeException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ParserTest {
+    @Test
+    public void parse_validTodoCommand_success() throws DukeException {
+        Command cmd = Parser.parse("todo read book");
+        assertTrue(cmd instanceof TodoCommand);
+    }
+
+    @Test
+    public void parse_validExitCommand_success() throws DukeException {
+        Command cmd = Parser.parse("bye");
+        assertTrue(cmd instanceof ExitCommand);
+    }
+
+    @Test
+    public void parse_invalidCommand_exceptionThrown() {
+        assertThrows(DukeException.class, () -> {
+            Parser.parse("invalidCommandWord");
+        });
+    }
+
+    @Test
+    public void parse_emptyInput_exceptionThrown() {
+        assertThrows(DukeException.class, () -> {
+            Parser.parse("   ");
+        });
+    }
+}
